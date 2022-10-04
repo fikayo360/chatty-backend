2a2ca0a40c74b026ef34c69d1dd9f89acab2a696
fikayo <fikayoadele@gmail.com>
2022-10-04 21:37:50 +0100

    updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const port = process.env.PORT || 5000;
+const port =  5000;
 const connectDB = require("./db/connect")
 require('dotenv').config();
 
